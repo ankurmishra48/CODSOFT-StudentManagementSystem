7220258d5227c71b2c4f0aa01e1730aec4f373b1
ANKUR MISHRA <113233185+ankurmishra48@users.noreply.github.com>
2024-06-06 23:16:08 +0530

    Create ViewStudent.java

---
+++ ViewStudent.java
@@ -0,0 +1,104 @@
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+import java.awt.event.*;
+import net.proteanit.sql.DbUtils;
+
+public class ViewStudent extends JFrame implements ActionListener {
+
+    JTable table;
+    Choice StudentId;
+    JButton search, print, update, back;
+
+    ViewStudent() {
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel searchlbl = new JLabel("Search by Student Id");
+        searchlbl.setBounds(20, 20, 150, 20);
+        add(searchlbl);
+        
+        StudentId = new Choice();
+        StudentId.setBounds(180, 20, 150, 20);
+        add(StudentId);
+        
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from student");
+            while(rs.next()) {
+                StudentId.add(rs.getString("stuId"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        table = new JTable();
+        
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from student");
+            table.setModel(DbUtils.resultSetToTableModel(rs));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        JScrollPane jsp = new JScrollPane(table);
+        jsp.setBounds(0, 100, 900, 600);
+        add(jsp);
+        
+        search = new JButton("Search");
+        search.setBounds(20, 70, 80, 20);
+        search.addActionListener(this);
+        add(search);
+        
+        print = new JButton("Print");
+        print.setBounds(120, 70, 80, 20);
+        print.addActionListener(this);
+        add(print);
+        
+        update = new JButton("Update");
+        update.setBounds(220, 70, 80, 20);
+        update.addActionListener(this);
+        add(update);
+        
+        back = new JButton("Back");
+        back.setBounds(320, 70, 80, 20);
+        back.addActionListener(this);
+        add(back);
+        
+        setSize(900, 700);
+        setLocation(300, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == search) {
+            String query = "select * from student where stuId = '"+StudentId.getSelectedItem()+"'";
+            try {
+                Conn c = new Conn();
+                ResultSet rs = c.s.executeQuery(query);
+                table.setModel(DbUtils.resultSetToTableModel(rs));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == print) {
+            try {
+                table.print();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == update) {
+            String selectedStuId = StudentId.getSelectedItem();
+            setVisible(false);
+            new UpdateStudent(selectedStuId);
+        } else {
+            setVisible(false);
+            new Home();
+        }
+    }
+
+    public static void main(String[] args) {
+        new ViewStudent();
+    }
+}
