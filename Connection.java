ef96827d23d622aad07b0d1d7da94f2ab78083b2
ANKUR MISHRA <113233185+ankurmishra48@users.noreply.github.com>
2024-06-06 23:13:48 +0530

    Create Connection.java

---
+++ Connection.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///studentmanagement", "root", "0408");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
